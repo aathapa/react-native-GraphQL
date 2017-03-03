26f269c4f4237b148856df8b1a2776ff5bca8810
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-03-03 14:55:10 +0545

    main app directory

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import AppRouter from './Routes';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <AppRouter />
+        );
+    }
+}
